d17e804def8dc12a82f2000f702a2d3af97eaf0f
elgharbi <elgharbiay@gmail.com>
2024-01-03 21:09:17 +0100

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
+  return promise
+    .then(() => ({ status: 200, body: "success" }))
     .catch(() => Error())
     .finally(() => {
-      console.log('Got a response from the API');
+      console.log("Got a response from the API");
     });
 }
