c94332f82d7a336cdbcf07e97dd042349b37f68f
elgharbi <elgharbiay@gmail.com>
2024-01-04 00:49:54 +0100

    Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
