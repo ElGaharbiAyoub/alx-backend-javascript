f087464b72e1f2e994ed55c86c5c6c0b7ad080a1
elgharbi <elgharbiay@gmail.com>
2024-01-05 01:45:26 +0100

    Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,23 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
