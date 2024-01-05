941e1d57b8e637271266a1106607b1eceb151f11
elgharbi <elgharbiay@gmail.com>
2024-01-05 01:30:00 +0100

    done

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (students.some((student) => typeof student !== 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (students.some((student) => typeof student !== 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
+}
