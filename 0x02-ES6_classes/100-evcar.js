c5b5b5a847d01a71c43792f647aa305f59faf60d
elgharbi <elgharbiay@gmail.com>
2024-01-05 02:02:51 +0100

    Add EVCar class and test file

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
+}
