e6c8b349cc29b306978afc1bfb99c3c470ca7c55
elgharbi <elgharbiay@gmail.com>
2024-02-27 01:32:16 +0100

    Refactor stdin handling in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,16 @@
-#!/usr/bin/node
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
+process.stdin.setEncoding('utf8');
 if (process.stdin.isTTY) {
-  rl.once('line', (input) => {
-    console.log(`Your name is: ${input}`);
+  process.stdin.on('data', (name) => {
+    process.stdout.write(`Your name is: ${name}`);
     process.exit();
   });
 } else {
-  rl.once('line', (input) => {
-    console.log(`Your name is: ${input}`);
+  process.stdin.on('data', (name) => {
+    process.stdout.write(`Your name is: ${name}`);
     process.exit();
   });
-
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
