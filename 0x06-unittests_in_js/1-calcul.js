4af0d650a65c1ff0bfe32aef1fb258e95f236347
elgharbi <elgharbiay@gmail.com>
2024-02-29 00:17:57 +0100

    Add calculateNumber function and unit tests

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  } else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } else if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+}
+
+module.exports = calculateNumber;
