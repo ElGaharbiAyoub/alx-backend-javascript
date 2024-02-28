e89bc503515068395a11d84d12a2fcfa2f9da28d
elgharbi <elgharbiay@gmail.com>
2024-02-28 22:46:09 +0100

    Combining descriptions

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', function () {
+  it('should return 4 when adding 1 and 3', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return 5 when adding 1 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5 when adding 1.2 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6 when adding 1.5 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
