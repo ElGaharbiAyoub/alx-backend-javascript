378fbf1a1de9e5d694f362023aaed4b516695cb9
elgharbi <elgharbiay@gmail.com>
2024-02-29 16:20:13 +0100

    Add assertion using assert module in 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,13 @@
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
+const assert = require('assert');
 
 describe('sendPaymentRequestToApi', function () {
   it('should call Utils.calculateNumber with the correct arguments', function () {
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20);
+    assert(calculateNumberSpy.calledWith('SUM', 100, 20));
     calculateNumberSpy.restore();
   });
 });
