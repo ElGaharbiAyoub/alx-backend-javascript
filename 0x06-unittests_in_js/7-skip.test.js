d08ce44912cb4480b9ba47f372948998bde377aa
elgharbi <elgharbiay@gmail.com>
2024-02-29 19:04:51 +0100

    Fix test case for equality comparison

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,8 +9,8 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
-    expect(1 === 3).to.be.false;
+  it.skip('1 is equal to 3', () => {
+    expect(1 === 3).to.be.true;
   });
 
   it('3 is equal to 3', () => {
