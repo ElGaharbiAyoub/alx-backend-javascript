39df61ca118456bd6b62f2a6d4c329dcc0daeb5b
elgharbi <elgharbiay@gmail.com>
2024-02-29 17:24:32 +0100

    Update test case for sendPaymentRequestToApi function

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,7 +4,7 @@ const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', function () {
   it('should call Utils.calculateNumber with the correct arguments', function () {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const calculateNumberSpy = sinon.spy(console, 'log');
     const subt = sinon.stub(Utils, 'calculateNumber').returns(10);
     sendPaymentRequestToApi(100, 20);
     sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20);
