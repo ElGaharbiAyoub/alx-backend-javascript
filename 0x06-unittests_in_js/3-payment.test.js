bb074a171e07bbe739f335991dca39c20710c572
elgharbi <elgharbiay@gmail.com>
2024-02-29 16:15:30 +0100

    Refactor unit tests for sendPaymentRequestToApi function

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,20 +2,28 @@
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
-const { expect } = require('chai');
 const assert = require('assert');
 
 describe('sendPaymentRequestToApi', function () {
-  it('should call the Utils function', function () {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  it('should return 120 if the calculateNumber is called', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.withArgs('SUM', 100, 20).returns(120);
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledOnce).to.be.true;
-    calculateNumberSpy.restore();
+    assert(calculateNumberStub.calledOnce);
+    calculateNumberStub.restore();
   });
-  it('should call Utils with the right arguments', function () {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-    calculateNumberSpy.restore();
+  it('should return 20 if the calculateNumber is called', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.withArgs('SUBTRACT', 100, 80).returns(20);
+    sendPaymentRequestToApi(100, 80);
+    assert(calculateNumberStub.calledOnce);
+    calculateNumberStub.restore();
+  });
+  it('should return Error if the calculateNumber is called', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.withArgs('DIVIDE', 100, 0).returns('Error');
+    sendPaymentRequestToApi(100, 0);
+    assert(calculateNumberStub.calledOnce);
+    calculateNumberStub.restore();
   });
 });
